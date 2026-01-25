fd7e9490ee737ce7af90129b7d7faca25c184f3d
Jorge Azevedo <jorgeazesouza2306@gmail.com>
2026-01-25 00:48:15 +0000

    modifica a frase

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
 function Home() {
-    return <h1>Gaby, eu amo você. Se você me ama dá uma risadinha!</h1>
+    return <h1>Gaby, eu amo você. Queria te dar vários beijinhos e muitas mordidinhas!</h1>
 }
 
 export default Home;
